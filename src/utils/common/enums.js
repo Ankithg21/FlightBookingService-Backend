44e83bae962a2dff61d1fcaaf820f5d48fd07056
Ankith G <ankithg2104@gmail.com>
2025-06-11 20:19:26 +0530

    Added Booking Model schema

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSSINESS: "Bussiness",
+    ECONOMY: "Economy",
+    PREMIUM_ECONOMY: "Premium-economy",
+    FIRST_CLASS: "First-class"
+}
+
+const BOOKING_STATUS = {
+    BOOKED: 'Booked',
+    CANCELLED: 'Cancelled',
+    INITIATED: 'Initiated',
+    PENDING: 'Pending'
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS,
+}
